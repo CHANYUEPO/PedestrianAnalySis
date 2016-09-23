a457320035405a5119642eb1465150f0649af463
tianmatong <1269079025@qq.com>
2016-09-24 04:39:49 +0800

    change class name in file objetector.h

--- objdetector.h
+++ objdetector.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <opencv2/opencv.hpp>
-class objdector
+class objdetector
 {
 public:
-	objdector() {}
-	virtual ~objdector() {}
+	objdetector() {}
+	virtual ~objdetector() {}
 	virtual void  detect(const cv::Mat & frame, std::vector<cv::Rect>& box) {}
 };

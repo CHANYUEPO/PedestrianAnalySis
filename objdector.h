52d0c1a6c585f13cc1ae74c96b4b3bce916825bd
tianmatong <1269079025@qq.com>
2016-09-24 04:25:10 +0800

    rm objector.h

--- objdector.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-class objdector
-{
-public:
-	objdector() {}
-	virtual ~objdector() {}
-	virtual void  detect(const cv::Mat & frame, std::vector<cv::Rect>& box) {}
-};
